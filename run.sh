eefceafbab2652aa4fdf207ea0c153169db39bf9
mickamy <t.mikami1992@gmail.com>
2024-01-01 08:54:35 +0900

    chore: refactor run.sh

--- run.sh
+++ run.sh
@@ -8,48 +8,47 @@ then
   exit 1
 fi
 
-check_message="start application w/ ${app_lang}..."
-
 echo "app_lang: $app_lang"
 
 function run_ruby() {
+  cd "/home/ishocon/webapp/$app_lang"
   bundle install
   unicorn -c unicorn_config.rb
-  echo "$check_message"
+  echo $check_message
 }
 
 function run_python() {
+  cd "/home/ishocon/webapp/$app_lang"
   /home/ishocon/.pyenv/shims/uwsgi --ini app.ini
-  echo "$check_message"
 }
 
 function run_python_sanic() {
-  /home/ishocon/.pyenv/shims/uwsgi --ini app.ini
-  echo "$check_message"
+  cd "/home/ishocon/webapp/${app_lang}_sanic"
+  /home/ishocon/.pyenv/shims/uwsgi --ini "app.ini"
 }
 
 
 function run_go() {
+  cd "/home/ishocon/webapp/$app_lang"
   go get -t -d -v ./...
   go build -o webapp *.go
   ./webapp
-  echo "$check_message"
 }
 
 function run_php() {
+  cd "/home/ishocon/webapp/$app_lang"
   sudo mv -f /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
   sudo cp webapp/php/php-nginx.conf /etc/nginx/nginx.conf
   sudo service nginx reload
-  echo "$check_message"
 }
 
 function run_crystal() {
+  cd "/home/ishocon/webapp/$app_lang"
   shards install
   crystal app.cr
-  echo "$check_message"
 }
 
-echo "run $app_lang app..."
-cd "/home/ishocon/webapp/$app_lang"
+echo "start running $app_lang app..."
+"run_${app_lang}"
+echo "completed to start running $app_lang app..."
 
-"run_$app_lang"
