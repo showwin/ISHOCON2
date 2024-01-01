70c76ce11e3da78a88d7bd0ea2d343e5db100820
mickamy <t.mikami1992@gmail.com>
2024-01-01 14:33:54 +0900

    Revert "bench: add sudo to entrypoint of benchmark"
    This reverts commit bfa978afa55c5a49d4040175ccbdeb03d7e704a0.

--- docker/benchmarker/entrypoint.sh
+++ docker/benchmarker/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -ux
 
-sudo service mysql start  # なぜか失敗する(調査中)
-sudo chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-sudo service mysql start  # 正しく起動
-sudo mysql -u root -pishocon -e 'CREATE DATABASE IF NOT EXISTS ishocon2;' && \
-sudo mysql -u root -pishocon -e "CREATE USER IF NOT EXISTS ishocon IDENTIFIED BY 'ishocon';" && \
-sudo mysql -u root -pishocon -e 'GRANT ALL ON *.* TO ishocon;' && \
-cd /root/admin && tar -jxvf ishocon2.dump.tar.bz2 && sudo mysql -u root -pishocon ishocon2 < /root/admin/ishocon2.dump
+service mysql start  # なぜか失敗する(調査中)
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+service mysql start  # 正しく起動
+mysql -u root -pishocon -e 'CREATE DATABASE IF NOT EXISTS ishocon2;' && \
+mysql -u root -pishocon -e "CREATE USER IF NOT EXISTS ishocon IDENTIFIED BY 'ishocon';" && \
+mysql -u root -pishocon -e 'GRANT ALL ON *.* TO ishocon;' && \
+cd /root/admin && tar -jxvf ishocon2.dump.tar.bz2 && mysql -u root -pishocon ishocon2 < /root/admin/ishocon2.dump
 
 echo 'setup completed.'
 
